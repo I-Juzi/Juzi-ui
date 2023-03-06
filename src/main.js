bc6b94cef0c96585ee0b0a5ce928ac1d811e030e
root <837356753@qq.com>
2023-03-06 17:19:46 +0800

    1、添加图标库 2、添加elementui 3、添加echarts

--- src/main.js
+++ src/main.js
@@ -3,13 +3,24 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+// 导入 ElementUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+//图标库
+import 'font-awesome/css/font-awesome.min.css'
+//引入 echarts
+import * as echarts from 'echarts'
+//挂载
+Vue.prototype.$echarts = echarts
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
